550dafb113a19568c09299cc4a5a619277d9dcd8
datalyrics <info@datalyrics.de>
2025-09-02 23:10:24 +0200

    remove warning for unused parameter with an underscore

--- my_app/web/controllers/topic_controller.ex
+++ my_app/web/controllers/topic_controller.ex
@@ -3,10 +3,17 @@ defmodule MyApp.TopicController do
 
   alias MyApp.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
   end
 
+  def create(conn, %{"topic" => topic}) do
+    IO.inspect(topic)
+
+
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
+  end
 end
